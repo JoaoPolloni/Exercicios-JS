c7f02fcf3f148f1999f30ef2408684ab118da14b
JoãoPolloni <jpolloni16@gmail.com>
2023-01-19 17:56:14 -0300

    descriçao

--- Notas.js
+++ Notas.js
@@ -1,1 +1,9 @@
-/* TEste*/
+/* ### Transformar notas escolares  
+Converte os numeros das notas para letras na classificação 
+* de 90 para cima - A
+* entre 80 - 89 - B
+* entre 70 - 79 - C
+* entre 60 - 69 - D
+* menor que 60 - F
+*/
+
